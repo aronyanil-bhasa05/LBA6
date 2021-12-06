27f579a67510a3570f1c9e53b06a80caf13e82a3
aronyanil-bhasa05 <bhasa05@gmail.com>
2021-12-06 22:02:26 +0530

    pushing first changes

---
+++ LBA6/LabBasedAssignment6/factorial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace LabBasedAssignment6
+{
+    class factorial
+    {
+        static void Main()
+        {
+            factorial facto= new factorial();
+            Console.Write("ENTER A NUMBER = ");
+            double n = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"FACTORIAL OF {n} IS {facto.fact(n)} ");
+            Console.ReadLine();
+        }
+
+        double fact (double num)
+        {
+            if (num == 0)
+                return 1;
+            else
+                return (num*fact(num - 1));
+        }
+
+        
+
+    }
+}
